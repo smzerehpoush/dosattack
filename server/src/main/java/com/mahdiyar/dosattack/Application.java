f678de70387d37b63d9138602d85407347d93eac
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-01-05 22:22:36 +0330

    add mongodb and redis configuration

--- server/src/main/java/com/mahdiyar/dosattack/Application.java
+++ server/src/main/java/com/mahdiyar/dosattack/Application.java
@@ -3,8 +3,11 @@ package com.mahdiyar.dosattack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Seyyed Mahdiyar Zerehpoush
@@ -13,6 +16,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 @EnableScheduling
 @EnableAsync
+@EnableSwagger2
+@EnableRedisRepositories
+@EnableMongoRepositories
 public class Application {
 
     public static void main(String[] args) {
--- server/src/main/java/com/mahdiyar/dosattack/config/SwaggerConfig.java
+++ server/src/main/java/com/mahdiyar/dosattack/config/SwaggerConfig.java
@@ -11,7 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
