d9fcc5e249c261d9b2dc7f2bdf2498dfa169cd16
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-01-12 11:30:32 +0330

    add db-init.sh and db-start.sh

--- server/db-start.sh
+++ server/db-start.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-docker pull redis
-docker run -d --name da-redis -p 27026:6379 redis
-docker pull mongo
-docker run -d --name da-mongo -p 27027:27017 mongo
+docker restart da-redis
+docker restart da-mongo
