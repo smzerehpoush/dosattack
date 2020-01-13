4127d88b3fcdcb81c95e35fdf7486f55077dfdf1
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-01-13 15:56:22 +0330

    add mysql to db init and start files

--- server/db-init.sh
+++ server/db-init.sh
@@ -3,3 +3,5 @@ docker pull redis
 docker run -d --name da-redis -p 27026:6379 redis
 docker pull mongo
 docker run -d --name da-mongo -p 27027:27017 mongo
+docker pull mysql
+docker run -d --name da-mysql -p 27028:3306 --env="MYSQL_ROOT_PASSWORD=1353" mysql
--- server/db-start.sh
+++ server/db-start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker restart da-redis
 docker restart da-mongo
+docker restart da-mysql
