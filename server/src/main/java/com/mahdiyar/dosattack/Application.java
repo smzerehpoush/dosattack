94c7f81a46c498606e138dbdd75f4da8d65fe103
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-01-13 17:00:07 +0330

    refactor

--- server/src/main/java/com/mahdiyar/dosattack/Application.java
+++ server/src/main/java/com/mahdiyar/dosattack/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Seyyed Mahdiyar Zerehpoush
