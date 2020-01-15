ef7e995a0a36032e793b17e1b2fb9d39789c58fd
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-01-15 12:02:29 +0330

    add Constatns

---
+++ server/src/main/java/com/mahdiyar/dosattack/util/Constants.java
@@ -0,0 +1,11 @@
+package com.mahdiyar.dosattack.util;
+
+/**
+ * @author Seyyed Mahdiyar Zerehpoush
+ */
+public class Constants {
+    public static String ADDRESS = "http://localhost:8099/api/v1/test";
+    public static String SIGNUP_URL = "http://localhost:8099/api/v1/user/signup";
+    public static String LOGIN_URL = "http://localhost:8099/api/v1/user/login";
+
+}
