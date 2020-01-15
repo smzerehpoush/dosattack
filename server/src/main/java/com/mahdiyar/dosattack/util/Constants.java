f64d0fcdbca72844658257d9e6623d2e3fe22ce7
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-01-15 14:45:59 +0330

    add base url

--- server/src/main/java/com/mahdiyar/dosattack/util/Constants.java
+++ server/src/main/java/com/mahdiyar/dosattack/util/Constants.java
@@ -4,9 +4,17 @@ package com.mahdiyar.dosattack.util;
  * @author Seyyed Mahdiyar Zerehpoush
  */
 public class Constants {
-    public static String ADDRESS = "http://localhost:8099/api/v1/test";
-    public static String SIGNUP_URL = "http://localhost:8099/api/v1/user/signup";
-    public static String LOGIN_URL = "http://localhost:8099/api/v1/user/login";
-    public static String AUTHORIZATION = "Authorization";
+
+    public static final String BASE_URL = "http://localhost:8099/api";
+    public static final String LOAN_REQUEST_URL = BASE_URL + "/v1/loan";
+    public static final String GET_BANKS_URL = BASE_URL + "/v1/bank";
+    public static final String ADDRESS = BASE_URL + "/v1/test";
+    public static final String SIGNUP_URL = BASE_URL + "/v1/user/signup";
+    public static final String LOGIN_URL = BASE_URL + "/v1/user/login";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String COOKIE = "Cookie";
+
+    private Constants() {
+    }
 
 }
