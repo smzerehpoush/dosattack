57602ebc9985f7cbdf56ec3151d62686d1517df2
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2020-01-15 14:46:46 +0330

    add NoArg Constructor

--- server/src/main/java/com/mahdiyar/dosattack/model/dto/request/loan/LoanRequestDto.java
+++ server/src/main/java/com/mahdiyar/dosattack/model/dto/request/loan/LoanRequestDto.java
@@ -1,11 +1,13 @@
 package com.mahdiyar.dosattack.model.dto.request.loan;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * @author Seyyed Mahdiyar Zerehpoush
  */
 @Data
+@AllArgsConstructor
 public class LoanRequestDto {
     private String userId;
     private String bankId;
